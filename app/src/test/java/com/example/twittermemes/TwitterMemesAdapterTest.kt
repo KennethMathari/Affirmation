7567327194c2820be9fddc2ff77049dea5e3b4e7
Kenneth Mathari <mathari580@gmail.com>
2022-01-28 14:04:18 +0300

    Tested the Adapter

--- app/src/test/java/com/example/twittermemes/TwitterMemesAdapterTest.kt
+++ app/src/test/java/com/example/twittermemes/TwitterMemesAdapterTest.kt
@@ -1,4 +1,23 @@
 package com.example.twittermemes
 
+import android.content.Context
+import com.example.twittermemes.adapter.ItemAdapter
+import com.example.twittermemes.model.TwitterMemes
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
 class TwitterMemesAdapterTest {
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size(){
+        val data = listOf(
+            TwitterMemes(R.string.affirmation1, R.drawable.image1),
+            TwitterMemes(R.string.affirmation2, R.drawable.image2)
+        )
+
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
 }
